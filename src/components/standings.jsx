df4c865f48830330f277f437f18bc665d2393458
Brendan Higgins <Bshannon@Briannas-MacBook-Air.local>
2019-04-11 11:06:57 -0400

    Fix bug

--- src/components/standings.jsx
+++ src/components/standings.jsx
@@ -10,13 +10,7 @@ const COL_DEFS = [
     cellStyle: { textAlign: 'start' },
     width: 200
   },
-  { headerName: 'SCORE', field: 'score' },
-  { headerName: 'PLAYER 1', field: 'player1', width: 150 },
-  { headerName: 'PLAYER 2', field: 'player2', width: 150 },
-  { headerName: 'PLAYER 3', field: 'player3', width: 150 },
-  { headerName: 'PLAYER 4', field: 'player4', width: 150 },
-  { headerName: 'PLAYER 5', field: 'player5', width: 150 },
-  { headerName: 'PLAYER 6', field: 'player6', width: 150 }
+  { headerName: 'SCORE', field: 'score' }
 ];
 
 export class Standings extends React.Component {
@@ -41,7 +35,7 @@ export class Standings extends React.Component {
         className='ag-theme-balham'
         style={{
           height: '500px',
-          width: '1200px'
+          width: '300px'
         }}
       >
         <AgGridReact
@@ -62,24 +56,9 @@ export class Standings extends React.Component {
               .map(id =>
                 this.props.players[id] ? this.props.players[id].total : 0
               )
-              .reduce((acc, val) => acc + val),
-            player1: this.getName(0, team),
-            player2: this.getName(1, team),
-            player3: this.getName(2, team),
-            player4: this.getName(3, team),
-            player5: this.getName(4, team),
-            player6: this.getName(5, team)
+              .reduce((acc, val) => acc + val)
           }))
           .sort((a, b) => a.score - b.score)
       : [];
-  };
-
-  getName = (num, team) => {
-    if (!this.props.players) {
-      return '';
-    }
-    const id = team.players[num];
-    const player = this.props.players[id];
-    return player ? player.fullName : '';
   };
 }
